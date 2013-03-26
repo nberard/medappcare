eefa70b3997fc05a5e02f622002002236edbcf08
n.berard <n.berard@meetic-corp.com>
2013-03-26 11:40:26 +0100

    - first test for import

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,8 +19,55 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+        echo 'aaaa';
+//        $this->load->helper('assets');
 		$this->load->view('welcome_message');
 	}
+
+    public function importAndroid()
+    {
+        $clientToken = '121bc1910a119f65f19910356ca8faaaf9bb9dd3';
+        $params = array(
+            'd=month',
+            'c=12',
+            'num=5',
+            't=popular',
+            'app_info=extended',
+            'client_token='.$clientToken,
+        );
+        $url = 'http://dev.appaware.com/1/app/top.json?'.implode('&', $params);
+        $returnDecoded = json_decode(file_get_contents($url), true);
+        echo 'call <br/><h2>'.$url."</h2><br/>";
+        echo 'nb results : '.count($returnDecoded['results'])."<br/><br/>";
+        foreach ($returnDecoded['results'] as $entry)
+        {
+            var_dump($entry);
+            echo $entry['name'].'['.$entry['package_name']."]<br/>";
+            $url = 'http://dev.appaware.com/1/app/show.json?p='.$entry['package_name'].'&client_token='.$clientToken;
+            $details = json_decode(file_get_contents($url), true);
+            var_dump($details);
+        }
+    }
+
+    public function importApple()
+    {
+        $url = 'https://itunes.apple.com/fr/rss/newapplications/limit=10/genre=6020/json';
+        $returnDecoded = json_decode(file_get_contents($url), true);
+//        var_dump($returnDecoded);
+        foreach ($returnDecoded['feed']['entry'] as $entry)
+        {
+            var_dump($entry);
+        }
+    }
+
+    function langTest()
+    {
+        // load language file
+        $this->lang->load('about');
+
+
+        $this->load->view('about');
+    }
 }
 
 /* End of file welcome.php */
