b0b26be024c1817af55b5825170362522ae40f8f
n.berard <n.berard@meetic-corp.com>
2013-04-11 18:59:04 +0200

    android feeder en cours

--- application/controllers/robot.php
+++ application/controllers/robot.php
@@ -6,15 +6,21 @@
  * Time: 18:21
  * @property http_call_manager $http_call_manager
  * @property apple_feeder $apple_feeder
+ * @property android_feeder $android_feeder
  */
 class Robot extends CI_Controller
 {
+
+    const ANDROID_TOKEN_API = '121bc1910a119f65f19910356ca8faaaf9bb9dd3';
 
     public function __construct()
     {
         parent::__construct();
         $this->load->library('log');
         $this->load->library('http_call_manager');
+        $this->load->model('Applications_model');
+        $this->load->model('Application_screenshots_model');
+        $this->load->model('Editeurs_model');
     }
 
     public function index()
@@ -24,44 +30,77 @@ class Robot extends CI_Controller
 
     public function android()
     {
-//        $this->http_call_manager->call('GET', '')
+        $website = 'http://dev.appaware.com/1/app/';
+        try
+        {
+            $result = $this->http_call_manager->call('GET',
+                'top.json',
+                '?d=month&t=popular&c=12&cc=worldwide&num=200&page=1&client_token='.self::ANDROID_TOKEN_API,
+                $website);
+
+            if(!empty($result["results"]))
+            {
+                $allAppsDetailed = array();
+                foreach($result["results"] as $app)
+                {
+                    $appDetailed = $this->http_call_manager->call('GET',
+                        'show.json',
+                        '?p='.$app['package_name'].'&client_token='.self::ANDROID_TOKEN_API,
+                        $website);
+                    $allAppsDetailed[] = $appDetailed;
+                }
+                $this->load->library('android_feeder', array($this->Applications_model, $this->Editeurs_model, $this->Application_screenshots_model, $allAppsDetailed));
+                try
+                {
+                    $this->android_feeder->feed('en', 'en');
+                }
+                catch(Exception $e)
+                {
+                    echo 'ERREUR : '.$e->getMessage();
+                }
+            }
+        }
+        catch(Exception $e)
+        {
+            $this->log->write_log('ERROR', $e->getMessage());
+        }
+
     }
 
     public function apple()
     {
         $langues = array('en', 'fr');
         $types = array('topfreeapplications', 'toppaidapplications', 'topgrossingapplications');
-        $this->load->model('Applications_model');
-        $this->load->model('Application_screenshots_model');
-        $this->load->model('Editeurs_model');
         foreach($langues as $langue)
         {
             foreach ($types as $type)
             {
                 try
                 {
-                    $result = $this->http_call_manager->call('GET', $type.'/limit=300/genre=6020', 'json', 'https://itunes.apple.com/'.$langue.'/rss');
+                    $result = $this->http_call_manager->call('GET',
+                                                            $type.'/limit=300/genre=6020',
+                                                            'json',
+                                                            'https://itunes.apple.com/'.$langue.'/rss');
+                    if(!empty($result['feed']['entry']))
+                    {
+                        $this->load->library('apple_feeder', array($this->Applications_model, $this->Editeurs_model, $this->Application_screenshots_model, $result['feed']['entry']));
+                        try
+                        {
+                            $this->apple_feeder->feed($langue, $langue);
+                        }
+                        catch(Exception $e)
+                        {
+                            echo 'ERREUR : '.$e->getMessage();
+                        }
+                    }
                 }
                 catch(Exception $e)
                 {
                     $this->log->write_log('ERROR', $e->getMessage());
                     continue;
                 }
-                if(!empty($result['feed']['entry']))
-                {
-                    $this->load->library('apple_feeder', array($this->Applications_model, $this->Editeurs_model, $this->Application_screenshots_model, $result['feed']['entry']));
-                    try
-                    {
-                        $this->apple_feeder->feed($langue, $langue);
-                    }
-                    catch(Exception $e)
-                    {
-                        echo 'ERREUR : '.$e->getMessage();
-                    }
-                }
             }
         }
-
     }
 }
 
---
+++ application/libraries/android_feeder.php
@@ -0,0 +1,72 @@
+<?php
+require_once APPPATH.'libraries/AppFeeder/ApplicationFeeder.php';
+class android_feeder extends ApplicationFeeder {
+
+    private $device;
+
+    public function __construct($_params)
+    {
+        parent::__construct($_params[0], $_params[1], $_params[2], $_params[3]);
+        $this->device = ApplicationFeeder::APPLICATION_DEVICE_ANDROID;
+    }
+
+    public function feed($_langue_store, $_langue_appli)
+    {
+        foreach($this->items as $item)
+        {
+            var_dump($item);exit;
+//            if(!$this->applicationModel->exists_applications(array('package' => )))
+//            {
+//                $editeur_id = $this->editeurModel->exists_editeurs(array('nom' => ));
+//                if(!$editeur_id)
+//                {
+//                    $editeur_id = $this->editeurModel->insert_editeurs(, );
+//                }
+//                $screens = array();
+//                $lien = '';
+//                foreach($item["link"] as $link)
+//                {
+//                    if(!empty($link['attributes']['im:assetType']) && $link['attributes']['im:assetType'] == "preview")
+//                    {
+//                        $screens[] = $link['attributes']['href'];
+//                    }
+//                    else $lien = $link['attributes']['href'];
+//                }
+//
+//                if($this->applicationModel->insert_applications(
+//                    $item["im:name"]["label"],
+//                    $item["id"]["attributes"]["im:bundleId"],
+//                    $this->device,
+//                    $item["title"]["label"],
+//                    $item["summary"]["label"],
+//                    $item["im:price"]["attributes"]["amount"],
+//                    $item["im:price"]["attributes"]["currency"],
+//                    $_langue_store,
+//                    $_langue_appli,
+//                    $editeur_id,
+//                    -1,
+//                    $lien,
+//                    $item["im:releaseDate"]["attributes"]["label"]))
+//                {
+//                    $application_id = $this->applicationModel->db->insert_id();
+//                    foreach($screens as $screen)
+//                    {
+//                        $this->applicationScreenshotModel->insert_application_screenshots($screen, $application_id);
+//                    }
+//                    echo $item["im:name"]["label"]." done <br/>";
+//                }
+//                else
+//                {
+//                    echo $item["im:name"]["label"]." failed <br/>";
+//                }
+//
+//            }
+//            else
+//            {
+//                echo 'package '.$item["im:name"]["label"].' already exists <br/>';
+//            }
+//            var_dump($item);exit;
+        }
+    }
+
+}
