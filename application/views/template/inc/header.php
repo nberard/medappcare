1b29f69022848b79eb97a45e1f811c5b6003d949
Jérémie Berduck <jeremie@omts.fr>
2013-04-16 19:59:23 +0200

    Initial work
    Adding some css and structures on the template...

---
+++ application/views/template/inc/header.php
@@ -0,0 +1,14 @@
+<div class="wrapper">
+    <h1>Medappcare</h1>
+    <div class="links particuliers">
+        <div class="social">
+            <a class="facebook" href="facebook.com">Rejoignez-nous sur Facebook !</a>
+            <a class="twitter" href="twitter.com">Toutes les infos sur Twitter</a>
+        </div>
+        <div class="meta">
+            <a href="#" class="pro">Espace Pro</a>
+            <a href="#" class="connexion">Connexion</a>
+            <a href="#" class="language">FR</a>
+        </div>
+    </div>
+</div>
---
+++ application/views/template/inc/menuParticulier.php
@@ -0,0 +1,22 @@
+<nav>
+    <ul>
+        <li class="home">
+            <a href="index.php">Home</a>
+        </li>
+        <li class="masante">
+            <a href="#">Ma Santé</a>
+        </li>
+        <li class="monquotidien">
+            <a href="#">Mon Quotidien</a>
+        </li>
+        <li class="minformer">
+            <a href="#">M'Informer</a>
+        </li>
+        <li class="medeplacer">
+            <a href="#">Me Déplacer</a>
+        </li>
+        <li class="search">
+            <a href="#">Rechercher</a>
+        </li>
+    </ul>
+</nav>
