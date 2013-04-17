05ef056fe069575096a88c394e9e5ada02ab61ef
Jérémie Berduck <jeremie@omts.fr>
2013-04-17 17:56:10 +0200

    Header
    Header completed (except for medicine men).

--- application/views/template/inc/menuParticulier.php
+++ application/views/template/inc/menuParticulier.php
@@ -4,16 +4,16 @@
             <a href="index.php">Home</a>
         </li>
         <li class="masante">
-            <a href="#">Ma Santé</a>
+            <a href="#"><span class="picto"></span><span class="text">Ma Santé<span></a>
         </li>
         <li class="monquotidien">
-            <a href="#">Mon Quotidien</a>
+            <a href="#"><span class="picto"></span><span class="text">Mon Quotidien</span></a>
         </li>
         <li class="minformer">
-            <a href="#">M'Informer</a>
+            <a href="#"><span class="picto"></span><span class="text">M'Informer</span></a>
         </li>
         <li class="medeplacer">
-            <a href="#">Me Déplacer</a>
+            <a href="#"><span class="picto"></span><span class="text">Me Déplacer</span></a>
         </li>
         <li class="search">
             <a href="#">Rechercher</a>
