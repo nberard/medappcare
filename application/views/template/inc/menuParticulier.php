0ec786c22bdabc91b464029805d4c7411691da23
Jérémie Berduck <jeremie@omts.fr>
2013-04-24 21:10:22 +0200

    Homepage (picto)
    Add all pict for OS and Notes

--- application/views/template/inc/menuParticulier.php
+++ application/views/template/inc/menuParticulier.php
@@ -3,17 +3,17 @@
         <li class="home">
             <a href="index.php">Home</a>
         </li>
-        <li class="masante megamenu">
-            <a href="masante.php"><span class="picto"></span><span class="text">Ma Santé<span></a>
+        <li class="navmasante megamenu">
+            <a href="navmasante.php"><span class="picto"></span><span class="text">Ma Santé<span></a>
         </li>
-        <li class="monquotidien megamenu">
-            <a href="monquotidien.php"><span class="picto"></span><span class="text">Mon Quotidien</span></a>
+        <li class="navmonquotidien megamenu">
+            <a href="navmonquotidien.php"><span class="picto"></span><span class="text">Mon Quotidien</span></a>
         </li>
-        <li class="minformer megamenu">
-            <a href="minformer.php"><span class="picto"></span><span class="text">M'Informer</span></a>
+        <li class="navminformer megamenu">
+            <a href="navminformer.php"><span class="picto"></span><span class="text">M'Informer</span></a>
         </li>
-        <li class="medeplacer megamenu">
-            <a href="medeplacer.php"><span class="picto"></span><span class="text">Me Déplacer</span></a>
+        <li class="navmedeplacer megamenu">
+            <a href="navmedeplacer.php"><span class="picto"></span><span class="text">Me Déplacer</span></a>
         </li>
         <li class="search">
             <a href="#">Rechercher</a>
