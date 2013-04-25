b482a59d25958f1efab2f258b637fc12dfb0adab
Jérémie Berduck <jeremie@omts.fr>
2013-04-25 18:24:53 +0200

    App Page & Divers
    Adding App detail page.
    Styles for app page.
    Adding an include for all pages for the heading.

--- application/views/template/app.php
+++ application/views/template/app.php
@@ -1,24 +1,6 @@
-<!doctype html>
-<html lang="en"> <!-- A CHANGER EN FONCTION DE LA LANGUE -->
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
-	<title>MEDAPPCARE</title>
+<?php include ('inc/header_meta.php') ; ?>
 
-	<meta name="description" content="">
-	<meta name="author" content="Medappcare">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-	
-	<link rel="stylesheet" type="text/css" media="all" href="css/stylesheet.css" />
-	
-	
-</head>
-
-<body class="homepage particuliers">
+<body class="app particuliers masante"> <!-- Attention à la catégorie ! -->
 
     <header id="header">
     
@@ -29,30 +11,76 @@
     </header>
     
     <div id="dropdown" class="loading"></div><!-- #dropdown Menu -->
-
-    <section id="slider"><?php include ('inc/home_slider.php') ; ?></section> <!-- Section Slider -->
     
-    <section id="selections"><?php include ('inc/widget_selection.php') ; ?></section> <!-- Section La Sélection Medappcare -->
-
-    <div class="colorsLine"></div>
-
-    <section id="listApps">
-    
+    <div class="title">
         <div class="wrapper">
+            <h2>Titre d'Application <a href="developer.php" title="Nom du Développeur"><span>par</span> Nom du studio</a></h2> <!-- INSÉRER LE TITRE DE L'APP ICI et le lien vers la page du Développeur-->
+        </div>
+    </div>
+    
+    <section id="metapp" class="catmasante">
+        <div class="wrapper">
+    
+        <div class="icone"></div>
         
-        <?php include ('inc/home_lasteval.php') ; ?> <!-- Section Les dernières apps évaluées -->
-        
-        <?php include ('inc/home_topfive.php') ; ?> <!-- Section Le Top 5 -->
+        <div class="content right">
+            <div class="appnote noteMedappcare"><span></span><a href="#thegrid" class="note deux">Deux</a></div>
+            <div class="appnote notePro"><span></span><a href="#thegrid" class="note neuf">Cinq</a></div>
+            <div class="appnote noteGens"><span></span><a href="#thegrid" class="note huit">Huit</a></div>
+        </div>
         
         </div>
+    </section>
+    
+    <section id="appSectionOne" class="catmasante">
+        <div class="wrapper">
+            <div class="sidebar left">
+                <div class="os">
+                    <div class="list"><span class="ios">iOS</span><span class="price">4,89 €</span></div> <!-- INSERER L'OS et le prix correspondant-->
+                    <div class="list"><span class="android">Android</span><span class="price">2,34 €</span></div>
+                    <div class="list"><span class="web">Web App</span><span class="price">gratuit</span></div>
+                </div>
+                <div class="buttons">
+                    <a href="#" class="noter">Noter l'Application</a>
+                    <a href="#" class="signaler">Signaler</a>
+                </div>
+            </div>
+            <div class="content right description">
+                <h3>Notre Avis</h3>
+                <p>Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Curabitur blandit tempus porttitor.</p>
+            </div>
+            <div class="clear"></div>
+        </div> <!-- end wrapper -->
+    </section>
+    
+    <div class="line"></div>
+    
+    <section id="appSectionTwo" class="catmasante">
+        <div class="wrapper">
+        <div class="sidebar left">
+            <div class="qrcode">
+                <img src="tmp/qr.png" alt="qr" width="200" height="200">
+                <span>Flashez le code ci-dessus pour télécharger l’application.</span>
+            </div>
+            <div class="social">
+                <div class="sharingTwitter"></div>
+                <div class="sharingFacebook"></div>
+                <div class="sharingGoogleplus"></div>
+            </div>
+        </div>
+        <div id ="thegrid" class="content right">
+            <h5 class="soon">Retrouvez prochainement ici la grille d'évaluation de l'application.
+        </div>
+        <div class="clear"></div>
+        </div> <!-- end wrapper -->
+    </section>
+    
+    <section id="appTabs" class="catmasante">
     
     </section>
     
     <section id="devices"><?php include ('inc/widget_devices.php') ; ?></section> <!-- Section Devices connectés -->
-    
-    <section id="news"><?php include ('inc/widget_news.php') ; ?></section> <!-- Section Actualité -->
-    
-    <section id="pushFooter"><?php include ('inc/home_pushpartners.php') ; ?></section> <!-- Section Push Information -->
     
     <section id="partners"><?php include ('inc/partners.php') ; ?></section> <!-- Section Partenaires -->
 
--- application/views/template/category.php
+++ application/views/template/category.php
@@ -1,22 +1,4 @@
-<!doctype html>
-<html lang="en"> <!-- A CHANGER EN FONCTION DE LA LANGUE -->
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
-	<title>MEDAPPCARE</title>
-
-	<meta name="description" content="">
-	<meta name="author" content="Medappcare">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-	
-	<link rel="stylesheet" type="text/css" media="all" href="css/stylesheet.css" />
-	
-	
-</head>
+<?php include ('inc/header_meta.php') ; ?>
 
 <body class="category particuliers masante">
 
