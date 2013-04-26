bf3d7d909ceb04a5d48e711516c69640cf623468
n.berard <n.berard@meetic-corp.com>
2013-04-26 10:43:16 +0200

    oups

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://http://medappcare.omts.fr/';
+$config['base_url']	= 'http://medappcare.omts.fr/';
 
 /*
 |--------------------------------------------------------------------------
