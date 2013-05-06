efc3315f7bd188adee36bd094656301eabf9b68d
djedie <jerome@omts.fr>
2013-05-06 12:48:26 +0200

    Modifs appels JS

---
+++ application/logs/log-2013-05-06.php
@@ -0,0 +1,76 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-05-06 11:37:56 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 11:37:56 --> Unable to connect to the database
+ERROR - 2013-05-06 11:37:57 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 11:37:57 --> Unable to connect to the database
+ERROR - 2013-05-06 11:38:00 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 11:38:00 --> Unable to connect to the database
+ERROR - 2013-05-06 11:38:00 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 11:38:00 --> Unable to connect to the database
+ERROR - 2013-05-06 11:41:31 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 11:41:31 --> Unable to connect to the database
+ERROR - 2013-05-06 11:41:31 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 11:41:31 --> Unable to connect to the database
+ERROR - 2013-05-06 11:45:42 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 11:45:42 --> Unable to connect to the database
+ERROR - 2013-05-06 11:45:43 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 11:45:43 --> Unable to connect to the database
+ERROR - 2013-05-06 12:10:54 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:10:54 --> Unable to connect to the database
+ERROR - 2013-05-06 12:10:54 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:10:54 --> Unable to connect to the database
+ERROR - 2013-05-06 12:23:50 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:23:50 --> Unable to connect to the database
+ERROR - 2013-05-06 12:23:50 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:23:50 --> Unable to connect to the database
+ERROR - 2013-05-06 12:23:54 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:23:54 --> Unable to connect to the database
+ERROR - 2013-05-06 12:23:54 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:23:54 --> Unable to connect to the database
+ERROR - 2013-05-06 12:23:56 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:23:56 --> Unable to connect to the database
+ERROR - 2013-05-06 12:23:56 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:23:56 --> Unable to connect to the database
+ERROR - 2013-05-06 12:23:56 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:23:56 --> Unable to connect to the database
+ERROR - 2013-05-06 12:23:56 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:23:56 --> Unable to connect to the database
+ERROR - 2013-05-06 12:26:57 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:26:57 --> Unable to connect to the database
+ERROR - 2013-05-06 12:26:57 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:26:57 --> Unable to connect to the database
+ERROR - 2013-05-06 12:26:58 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:26:58 --> Unable to connect to the database
+ERROR - 2013-05-06 12:26:58 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:26:58 --> Unable to connect to the database
+ERROR - 2013-05-06 12:27:02 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:27:02 --> Unable to connect to the database
+ERROR - 2013-05-06 12:27:02 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:27:02 --> Unable to connect to the database
+ERROR - 2013-05-06 12:28:17 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:28:17 --> Unable to connect to the database
+ERROR - 2013-05-06 12:28:17 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:28:17 --> Unable to connect to the database
+ERROR - 2013-05-06 12:28:19 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:28:19 --> Unable to connect to the database
+ERROR - 2013-05-06 12:28:19 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:28:19 --> Unable to connect to the database
+ERROR - 2013-05-06 12:28:19 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:28:19 --> Unable to connect to the database
+ERROR - 2013-05-06 12:28:19 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:28:19 --> Unable to connect to the database
+ERROR - 2013-05-06 12:28:19 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:28:19 --> Unable to connect to the database
+ERROR - 2013-05-06 12:28:19 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'c12medappcaresql'@'localhost' (using password: YES) /Users/jerome/Website/medappcare/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2013-05-06 12:28:19 --> Unable to connect to the database
+ERROR - 2013-05-06 12:32:17 --> 404 Page Not Found --> perso/apple-touch-icon-precomposed.png
+ERROR - 2013-05-06 12:32:17 --> 404 Page Not Found --> perso/apple-touch-icon.png
+ERROR - 2013-05-06 12:32:22 --> 404 Page Not Found --> perso/apple-touch-icon-precomposed.png
+ERROR - 2013-05-06 12:32:22 --> 404 Page Not Found --> perso/apple-touch-icon.png
+ERROR - 2013-05-06 12:35:41 --> 404 Page Not Found --> perso/register.php
+ERROR - 2013-05-06 12:35:41 --> 404 Page Not Found --> perso/register.php
+ERROR - 2013-05-06 12:35:44 --> 404 Page Not Found --> perso/register.php
+ERROR - 2013-05-06 12:35:44 --> 404 Page Not Found --> perso/register.php
+ERROR - 2013-05-06 12:36:03 --> 404 Page Not Found --> perso/bootstrap-datepicker
+ERROR - 2013-05-06 12:36:03 --> 404 Page Not Found --> perso/bootstrap-multiselect
