d407e7c85ffd96f881668cb2dba3cff6cc8c1b7c
djedie <jerome@omts.fr>
2013-05-06 16:49:41 +0200

    Renommages des pages template

---
+++ application/views/template/TODO-device.php
@@ -0,0 +1,15 @@
+<?php include ('inc/header_meta.php') ; ?>
+
+
+
+
+
+
+
+
+    <?php include ('inc/footer.php') ; ?>
+
+    <?php include ('inc/footer_meta.php') ; ?> <!-- Appels JS & Autres -->
+    
+</body>
+</html>
