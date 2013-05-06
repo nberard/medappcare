2bfe6f734485eff3f8f26fe5b5db5254105c2db5
Hugo Mingoia <hugomingoia@gmail.com>
2013-05-06 12:52:35 +0200

    empty commit

---
+++ application/logs/log-2013-05-06.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-05-06 12:50:32 --> 404 Page Not Found --> perso/app.php
+ERROR - 2013-05-06 12:51:30 --> 404 Page Not Found --> perso/bootstrap-datepicker
+ERROR - 2013-05-06 12:51:30 --> 404 Page Not Found --> perso/bootstrap-multiselect
