ff9bb86925e67df743bfa22fb3686cd7bc9e6f84
djedie <jerome@omts.fr>
2013-05-06 15:34:31 +0200

    tests de commit

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://medappcare.omts.fr/';
-//$config['base_url']	= 'http://localhost';
+//$config['base_url']	= 'http://medappcare.omts.fr/';
+$config['base_url']	= 'http://localhost';
 
 /*
 |--------------------------------------------------------------------------
