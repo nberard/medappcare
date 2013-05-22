99fde492a555c6448302cc46f84badcf926e4047
djedie <jerome@omts.fr>
2013-05-22 14:35:13 +0200

    creation template Espace Perso + pro

--- application/views/inc/home_pushpartners.php
+++ application/views/inc/home_pushpartners.php
@@ -1,24 +1,32 @@
 <div class="wrapper">
     <ul>
         <li class="medappcare">
-            <span class="picto"></span>
-            <h4>Évaluation Medappcare</h4>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+            <a href="" title="Évaluation Medappcare">
+            	<span class="picto"></span>
+            	<h4>Évaluation Medappcare</h4>
+            	<p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+            </a>
         </li>
         <li class="professionnels">
-            <span class="picto"></span>
-            <h4>Professionnels de Santé</h4>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+        	<a href="" title="Professionnels de Santé">
+            	<span class="picto"></span>
+            	<h4>Professionnels de Santé</h4>
+            	<p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+            </a>
         </li>
         <li class="dev">
-            <span class="picto"></span>
-            <h4>Développeurs</h4>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+        	<a href="" title="Développeurs">
+	            <span class="picto"></span>
+	            <h4>Développeurs</h4>
+	            <p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+	        </a>
         </li>
         <li class="partners">
-            <span class="picto"></span>
-            <h4>Partenaires</h4>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+          	<a href="" title="Partenaires">
+	            <span class="picto"></span>
+	            <h4>Partenaires</h4>
+	            <p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+          	</a>
         </li>
     </ul>
 </div>
