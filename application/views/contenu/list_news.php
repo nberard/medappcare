7c92bbb8d897b36a37297b797d841b3288729b58
Jérémie Berduck <jeremie@omts.fr>
2013-05-22 16:03:28 +0200

    Liste News
    Added some files !

--- application/views/contenu/list_news.php
+++ application/views/contenu/list_news.php
@@ -6,13 +6,15 @@
     </div>
 </div>
 
-<div class="wrapper articles">
+<div class="articles">
     <ul>
-        <li class="categorieNews"><!-- Liste des news --> <!-- AJOUTER LA CATÉGORIE DE LA NEWS POUR CHAQUE -->
+        <li class="wrapper unarticle lacategorie"><!-- Liste des news --> <!-- AJOUTER LA CATÉGORIE DE LA NEWS POUR CHAQUE -->
             <h2>Titre de la news</h2>
+            <div class="categorie">Posté dans <a href="#" title="Titre de la Catégorie">Titre de la Catégorie</a></div>
             <div class="date">le 23 mai 2013</div>
             <div class="content">
                 <p>Sed posuere consectetur est at lobortis. <strong>Nullam id dolor</strong> id nibh ultricies vehicula ut id elit. Duis mollis, <i>est non commodo</i> luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+                <img src="../../../assets/img/tmp/gundam.jpg" alt="gundam" class="alignright" />
                 <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
                 <ul>
                     <li>liste puce</li>
@@ -23,12 +25,61 @@
                     <li>liste numéro</li>
                     <li>liste numéro</li>
                 </ol>
-                <h3></h3>
-                <h4></h4>
-                <h5></h5>
-                <h6></h6>
-                
+                <h3>Titre 3</h3>
+                <h4>Titre 4</h4>
+                <h5>Titre 5</h5>
+                <h6>Titre 6</h6>
+                <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit tempus porttitor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.</p>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper.</p>
+                <img src="../../../assets/img/tmp/gundam.jpg" alt="gundam" class="alignleft" />
+                <p>Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper.</p>
+                <p>Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper.</p>
             </div>
         </li>
+        
+        <li class="wrapper unarticle lacategorie"><!-- Liste des news --> <!-- AJOUTER LA CATÉGORIE DE LA NEWS POUR CHAQUE -->
+            <h2>Titre de la news 2</h2>
+            <div class="categorie">Posté dans <a href="#" title="Titre de la Catégorie">Titre de la Catégorie</a></div>
+            <div class="date">le 23 mai 2013</div>
+            <div class="content">
+                <p>Sed posuere consectetur est at lobortis. <strong>Nullam id dolor</strong> id nibh ultricies vehicula ut id elit. Duis mollis, <i>est non commodo</i> luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+                <img src="../../../assets/img/tmp/gundam.jpg" alt="gundam" class="alignright" />
+                <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                <ul>
+                    <li>liste puce</li>
+                    <li>liste puce</li>
+                </ul>
+                <ol>
+                    <li>liste numéro</li>
+                    <li>liste numéro</li>
+                    <li>liste numéro</li>
+                </ol>
+                <h3>Titre 3</h3>
+                <h4>Titre 4</h4>
+                <h5>Titre 5</h5>
+                <h6>Titre 6</h6>
+                <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit tempus porttitor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.</p>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper.</p>
+                <img src="../../../assets/img/tmp/gundam.jpg" alt="gundam" class="alignleft" />
+                <p>Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper.</p>
+                <p>Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper.</p>
+            </div>
+        </li>
+        
     </ul>
+    
+    <div class="pagination">
+        <a href="#" class="previous">< Page Précédente</a>
+        <a href="#" class="numbers active">1</a>
+        <a href="#" class="numbers">2</a>
+        <a href="#" class="numbers">3</a>
+        <a href="#" class="previous">Page suivante ></a>
+    </div>
+    
 </div>
