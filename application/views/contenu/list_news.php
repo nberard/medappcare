9ab91c4dd37df1741d224cc2bc1c00a6ccebcfff
Jérémie Berduck <jeremie@omts.fr>
2013-05-23 11:58:00 +0200

    Some adjustments for list_news.php
    Add link for the news title.

--- application/views/contenu/list_news.php
+++ application/views/contenu/list_news.php
@@ -10,7 +10,7 @@
     <ul id="list-news">
         <?php foreach ($articles as $article): ?>
             <li class="wrapper unarticle <?php echo $article->nom_categorie ? strtolower($article->nom_categorie) : ''; ?>"><!-- Liste des news --> <!-- AJOUTER LA CATÉGORIE DE LA NEWS POUR CHAQUE -->
-                <h2><?php echo $article->titre; ?></h2>
+                <h2 class="titlenews"><a href="#" title="<?php echo $article->titre; ?>"><?php echo $article->titre; ?></a></h2>
                 <?php if($article->nom_categorie): ?>
                     <div class="categorie">Posté dans <a href="#" title="<?php echo $article->nom_categorie; ?>"><?php echo $article->nom_categorie; ?></a></div>
                 <?php endif; ?>
