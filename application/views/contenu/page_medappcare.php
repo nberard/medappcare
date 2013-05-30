172fbd026eb39be3961cbc3639235453ac6821a9
Jérémie Berduck <jeremie@omts.fr>
2013-05-30 18:13:07 +0200

    Divers
    CSS adjustments
    Adding some php files
    ...

---
+++ application/views/contenu/page_medappcare.php
@@ -0,0 +1,22 @@
+<section class="static">
+
+    <div class="wrapper">
+
+        <h2>Page Évaluation Medappcare</h2>
+        
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra mauris at velit aliquet at iaculis ipsum tincidunt. Curabitur ut arcu lectus. Cras vehicula facilisis nisl sit amet hendrerit. Donec dignissim, quam nec interdum bibendum, urna dui sodales lacus, a iaculis ipsum ante sed est. Praesent quis augue quis dui mattis suscipit. Fusce pulvinar lobortis lobortis. Mauris in arcu metus, eleifend elementum justo. Phasellus tincidunt, metus at tempus ultricies, est ligula pretium enim, a egestas justo urna non eros. Ut ullamcorper purus non sem eleifend dignissim. In imperdiet enim at lorem iaculis a commodo lectus sodales. Sed purus lorem, tempus quis ultrices vitae, sollicitudin at ante. Sed dapibus molestie rhoncus. Ut faucibus placerat magna et rhoncus.
+		</p>
+		<p>
+		Donec posuere urna in felis tincidunt congue. Cras pellentesque augue non nibh fermentum auctor. Ut mattis venenatis eleifend. Integer sit amet sodales mi. Integer metus ante, ultrices quis volutpat et, suscipit vel nibh. Vivamus porta tempus aliquam. Praesent quis magna ligula. Quisque sed molestie nibh. Nullam quis placerat sapien. Vivamus quis ipsum a nibh scelerisque aliquam. Aenean sed leo ornare dui hendrerit luctus ut id ipsum. Fusce non laoreet metus. Aenean mi odio, pellentesque et ornare eu, interdum nec nulla.
+		</p>
+		<p>
+		Suspendisse potenti. Vivamus tortor magna, pulvinar non viverra sit amet, tincidunt sed dui. Nullam consectetur purus quis odio fringilla feugiat. Integer nibh dui, aliquam a vestibulum sagittis, sollicitudin non nunc. In pulvinar dui adipiscing est tempus convallis. Curabitur et dolor massa, imperdiet dapibus ante. Mauris feugiat, erat ac faucibus laoreet, mi risus pharetra erat, et dictum nunc tortor feugiat lectus. Fusce eu libero odio. In facilisis enim non nunc congue mollis.
+		</p>
+		<p>
+		Aenean gravida, nisi ac aliquet ornare, nisl ante iaculis nibh, ultricies hendrerit enim diam non dolor. Mauris tincidunt erat non mi dapibus hendrerit. Duis luctus dictum fringilla. Phasellus eget mauris at magna vestibulum porttitor. Cras non augue ac orci viverra interdum. Curabitur vulputate, orci bibendum ultrices mollis, est magna blandit arcu, quis blandit lacus nulla eu metus. Nam libero sapien, blandit vel vestibulum in, feugiat a massa. Phasellus vitae lorem vitae quam iaculis tincidunt. Cras justo diam, ullamcorper in ullamcorper eget, interdum id ante. Fusce pellentesque tellus vel tortor ullamcorper blandit. Nulla arcu velit, ultricies eu viverra vitae, rhoncus vitae nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+		</p>
+
+    </div>
+
+</section>
---
+++ application/views/inc/widget_deviceapps.php
@@ -0,0 +1,11 @@
+<div class="allapps catmasante"> <!-- Insérer ici la class de la catégorie (ex : catmasante) -->
+    <h3>Applications compatibles avec ce produit</h3>
+    <div class="filter">
+        <a href="#" class="gratuit" title="Filtrer les apps gratuites"><span></span>gratuit</a>
+        <a href="#" class="payant actif" title="Filtrer les apps payantes"><span></span>€</a>
+    </div>
+        <?php echo $app_grid; ?>
+    <div class="metaFooter"><a href="<?php echo $categorie->link_all; ?>">voir tout ></a></div>
+    <div class="clear"></div>
+</div>
+<div class="clear"></div>
