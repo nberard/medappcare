eee229f5d0e0d40cbf671f5eda53dfd4e9d84120
Jérémie Berduck <jeremie@omts.fr>
2013-05-30 18:52:07 +0200

    Social Sharing (phase 1)
    Add social sharing on app's pages.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -258,3 +258,44 @@ $('#pagination-app').bootpag({
 //}).on("page", function(event, /* page number here */ num){
 //     $("#list-news").html("Page " + num); // some ajax content loading...
 //});
+
+
+
+/* SOCIAL SHARING */
+
+/* Twitter */
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+/* Google Plus */
+window.___gcfg = {lang: 'fr'};
+
+(function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+
+/* Facebook */
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1&appId=275708732454136";  // Modifier l'ID de la page
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
