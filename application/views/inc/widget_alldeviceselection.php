53ad976683b2281baa53a63c0ae4dd29e8be883f
Jérémie Berduck <jeremie@MacBook-Pro-de-Jeremie.local>
2013-07-12 15:58:51 +0200

    Widget All devices selection
    & some CSS fixes

---
+++ application/views/inc/widget_alldeviceselection.php
@@ -0,0 +1,11 @@
+<div class="allapps catmasante"> <!-- Insérer ici la class de la catégorie (ex : catmasante) -->
+    <h3>Toutes les applications dans <?php echo $selection->nom; ?></h3>
+<!--    <div class="filter">-->
+<!--        <a href="#" class="gratuit" title="Filtrer les apps gratuites"><span></span>gratuit</a>-->
+<!--        <a href="#" class="payant actif" title="Filtrer les apps payantes"><span></span>€</a>-->
+<!--    </div>-->
+        <?php echo $app_grid; ?>
+    <div class="metaFooter"><a href="#"></a></div>
+    <div class="clear"></div>
+</div>
+<div class="clear"></div>
