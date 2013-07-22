75d02dc88b2434a5eb4a92e832e1d13eb434ea1c
djedie <jerome@omts.fr>
2013-07-22 11:35:19 +0200

    Delete old Templates

--- application/views/template-A_SUPPRIMER/inc/menu/navmasante.php
+++
@@ -1,9 +0,0 @@
-<div class="whiteLine"></div>
-<div class="wrapper">
-
-    <ul>
-        <li><a href="">Test</a></li>
-    </ul>
-
-<div class="bigpicto"></div>
-</div> <!-- end wrapper -->
--- application/views/template-A_SUPPRIMER/inc/widget_selection.php
+++
@@ -1,9 +0,0 @@
-<div class="wrapper">
-    <h2>Nos Sélections</h2>
-    <ul>
-        <li><a href="#" class="">Description</a><span class="underline"></span></li>
-        <li><a href="#" class="">Description</a><span class="underline"></span></li>
-        <li><a href="#" class="">Description</a><span class="underline"></span></li>
-        <li><a href="#" class="">Description</a><span class="underline"></span></li>
-    </ul>
-</div>
